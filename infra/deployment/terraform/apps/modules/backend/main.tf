0fe0b1dee60763d9417ec0fff155f73ec4fdf346
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-07-20 11:24:37 -0400

    Enable geocoding-backend.googleapis.com and maps-backend.googleapis.com

--- infra/deployment/terraform/apps/modules/backend/main.tf
+++ infra/deployment/terraform/apps/modules/backend/main.tf
@@ -43,9 +43,6 @@ resource "google_apikeys_key" "backend" {
     api_targets {
       service = "geocoding-backend.googleapis.com"
     }
-    api_targets {
-      service = "maps-backend.googleapis.com"
-    }
   }
 }
 
