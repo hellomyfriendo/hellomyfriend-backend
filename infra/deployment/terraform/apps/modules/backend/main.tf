f0a78869066cea5136d5068cdc2519d8e321cb8b
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-07-26 15:41:29 -0400

    Remove vpc access egress attribute to backend Cloud Run

--- infra/deployment/terraform/apps/modules/backend/main.tf
+++ infra/deployment/terraform/apps/modules/backend/main.tf
@@ -123,10 +123,6 @@ resource "google_cloud_run_v2_service" "backend" {
         value = google_storage_bucket.wants_images.name
       }
     }
-
-    vpc_access {
-      egress = "PRIVATE_RANGES_ONLY"
-    }
   }
 
   depends_on = [
