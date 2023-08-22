8a213284a4e0ad5f7a1b48dacde8026ff5ab8f2e
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-08-22 08:52:15 -0400

    Use terraform instead of gcloud to create service identities

--- infra/deployment/terraform/bootstrap/modules/iam/artifact_registry_sa.tf
+++ infra/deployment/terraform/bootstrap/modules/iam/artifact_registry_sa.tf
@@ -1,20 +1,11 @@
-locals {
-  artifact_registry_sa = "service-${data.google_project.project.number}@gcp-sa-artifactregistry.iam.gserviceaccount.com"
-}
-
-# See https://cloud.google.com/artifact-registry/docs/ar-service-account
-resource "null_resource" "artifact_registry_sa" {
-  provisioner "local-exec" {
-    command = "gcloud beta services identity create --service \"artifactregistry.googleapis.com\" --project ${data.google_project.project.project_id}"
-  }
+resource "google_project_service_identity" "artifact_registry" {
+  provider = google-beta
+  project  = data.google_project.project.project_id
+  service  = "artifactregistry.googleapis.com"
 }
 
 resource "google_kms_crypto_key_iam_member" "artifact_registry_sa_confidential" {
   crypto_key_id = var.confidential_kms_crypto_key
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:${local.artifact_registry_sa}"
-
-  depends_on = [
-    null_resource.artifact_registry_sa
-  ]
+  member        = "serviceAccount:${google_project_service_identity.artifact_registry.email}"
 }
--- infra/deployment/terraform/bootstrap/modules/iam/secret_manager_sa.tf
+++ infra/deployment/terraform/bootstrap/modules/iam/secret_manager_sa.tf
@@ -1,20 +1,11 @@
-locals {
-  secret_manager_sa = "service-${data.google_project.project.number}@gcp-sa-secretmanager.iam.gserviceaccount.com"
-}
-
-# See https://cloud.google.com/secret-manager/docs/cmek#service-identity
-resource "null_resource" "secret_manager_sa" {
-  provisioner "local-exec" {
-    command = "gcloud beta services identity create --service \"secretmanager.googleapis.com\" --project ${data.google_project.project.project_id}"
-  }
+resource "google_project_service_identity" "secret_manager" {
+  provider = google-beta
+  project  = data.google_project.project.project_id
+  service  = "secretmanager.googleapis.com"
 }
 
 resource "google_kms_crypto_key_iam_member" "secret_manager_sa_confidential" {
   crypto_key_id = var.confidential_kms_crypto_key
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member        = "serviceAccount:${local.secret_manager_sa}"
-
-  depends_on = [
-    null_resource.secret_manager_sa
-  ]
+  member        = "serviceAccount:${google_project_service_identity.secret_manager.email}"
 }
